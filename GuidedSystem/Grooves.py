1d2c7e1c093b843885066a0644e14d923b595a97
AnshPrakash <anshprakash1997@gmail.com>
2019-12-02 15:51:51 +0530

    correct tangent code

--- GuidedSystem/Grooves.py
+++ GuidedSystem/Grooves.py
@@ -4,18 +4,16 @@ import drawCircles as dc
 import numpy as np
 from sympy import Point, Circle, Line, Ray
 
-def solve(d1,d2,v,origin):
+def solve(r1,r2,v,origin):
+  r = (r2 - r1)
   z = v[0]**2 + v[1]**2
-  r = (d2 - d1)
   d = z - (r**2)
   if d < -1e-9:
     return(-1)
   d = (abs(d))**0.5
-  a = (r*v[0] + v[1]*(d**0.5))/(z)
-  b = (r*v[1] - v[0]*(d**0.5))/(z)
-  print(origin[0],origin[1])
-  print(v)
-  c = d1 - (a*origin[0] + b*origin[1])
+  a = (r*v[0] + v[1]*d)/(z)
+  b = (r*v[1] - v[0]*d)/(z)
+  c = r1 - (a*origin[0] + b*origin[1])
   return(a,b,c)
 
 def getTangents(c1,c2,r1,r2):
@@ -33,28 +31,29 @@ def getTangents(c1,c2,r1,r2):
         tangents.append(line)
   return(tangents)
 
-def drawtangents(tangents):
+def drawtangents(tangents,circles):
   temp = np.zeros((605,641,3))
   for tan in tangents:
-    cv2.line(temp,(30,int(tan[0]*30 + tan[1]*30 + tan[2])),(600,int(tan[0]*600 + tan[1]*600 + tan[2])),(255,0,0),1)
+    cv2.line(temp,(-30,int(tan[0]*-30 + tan[1]*-30 + tan[2])),(1600,int(tan[0]*1600 + tan[1]*1600 + tan[2])),(255,0,0),1)
+  for cir in circles:
+    cv2.circle(temp,(cir[0],cir[1]),cir[2],(0,0,255),1)
   cv2.imshow("temp",temp)
   cv2.waitKey(0)
 
-p1,p2, p3 = Point(0, 0), Point(5, 5), Point(6, 0)
 
 # circles = dc.getCircles(sys.argv[1])
 # print(circles)
+# img = cv2.imread(sys.argv[1],cv2.IMREAD_UNCHANGED)
 
-img = cv2.imread(sys.argv[1],cv2.IMREAD_UNCHANGED)
 circles  = [[165, 411, 44], [414, 408, 80]]
-for cir in circles:
-  cv2.circle(img,(cir[0],cir[1]),cir[2],(0,0,255),1)
+# for cir in circles:
+#   cv2.circle(img,(cir[0],cir[1]),cir[2],(0,0,255),1)
 
 c1 = circles[0][:-1]
 c2 = circles[1][:-1]
 r1 = circles[0][2]
 r2 = circles[1][2]
-cv2.imshow("image",img)
+# cv2.imshow("image",img)
 tang = getTangents(c1,c2,r1,r2)
-drawtangents(tang)
+drawtangents(tang,circles)
 cv2.destroyAllWindows()
